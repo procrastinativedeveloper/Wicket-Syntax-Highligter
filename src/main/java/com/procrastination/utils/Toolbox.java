fef49794851fa03401e1c6bc28c97bee6c05e5ce
procrastinative.developer <procrastinative.developer@gmail.com>
2012-08-04 21:56:17 +0200

    add helper method emptyOrNull for Strings

--- src/main/java/com/procrastination/utils/Toolbox.java
+++ src/main/java/com/procrastination/utils/Toolbox.java
@@ -1,5 +1,20 @@
 package com.procrastination.utils;
 
-
+/**
+ * Helper method, here you can find some helper static method - I don't wanna add dependecy to commons or Guava when I need only few method
+ */
 public class Toolbox {
+
+    /**
+     * Helper class
+     */
+    private Toolbox() {
+        throw new RuntimeException("Cannot create instance of helper class");
+    }
+
+
+    public static boolean emptyOrNull(String toTest) {
+        return toTest == null || toTest.length() == 0;
+    }
+
 }
